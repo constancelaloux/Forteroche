aeb703467c51d65984eef953763b38b629ff3fae
Constance Laloux <constancelaloux@gmail.com>
2020-08-24 20:27:23 +0200

    make changes on views

--- src/blogenalaskaFram/views/Header.php
+++ src/blogenalaskaFram/views/Header.php
@@ -1,103 +1,45 @@
-<!--Menu-->
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light" id="navbar">
+        <!--bg-transparent-->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand" href="#">
+            <img src="/../../../public/images/logo.png" width="100" height="100" class="d-inline-block .align-middle " alt="logo">
+            Jean Forteroche
+        </a>
 
-<header class="navbarAuthor">
-
-    <div class="container">
-        <div class="navAuthor">
-        <nav class="navbar navbar-expand-lg navbar-dark col-lg-12 fixed-top">
-            
-<!--icon-->
-            <a class="navbar-brand" href="#">
-                <img src="/public/images/logoBackend.png" alt="logo">
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-<!--fin icon-->
-            
-<!--Le menu de navigation--> 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="/public/index.php?action=goToFrontPageOfTheBlog">Site web <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/public/index.php?action=mainBackendPage">Accueil</a>
-                    </li>
-
-<!--dropdown-->
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >Articles</a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="/public/index.php?action=writeAnArticle">Rédiger un article</a>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item active">
+                    <a class="nav-link" href="/">Accueil <span class="sr-only">(current)</span></a>
+                </li>
+                <?php 
+                if (isset($_SESSION['status'])):
+                    if($_SESSION['status'] === 'client'): ?>
+                        <div class="navbar-test">Connecté en tant que <?php echo $_SESSION['authorUsername'] ?></div>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Profil</a>
+                            <div class="dropdown-menu">
+                                <a class="dropdown-item" href="/updateuser&id=<?php echo $_SESSION['authorId']?>">Modifier son profil</a>
+                                <a class="dropdown-item" href="/deleteuser&id=<?php echo $_SESSION['authorId']?>">Supprimer son profil</a>
+                                <form class="nav-item active pr-1" method="post" action = "/logout">
+                                    <button class="btn btn-outline-success my-2 my-sm-0">Se déconnecter</button>
+                                </form>
                             </div>
-                    </li>
-
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"  data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >Commentaires<b class="caret"></b></a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="/public/index.php?action=getCommentsViewDatatables">Commentaires</a>
-                            </div>
-                    </li> 
-                </ul>
-
-<!--Click sur image pour déconnecter-->
-                <div class="d-flex">
-                    <div class="dropdown mr-1">
-                            <button class="btn btn-sm btn-secondary dropdown-toggle" type="button" id="dropdownMenuOffset" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-offset="10,20"  style="background-color: #23282d;">                                     
-                                <?php
-                                //Session
-                                try
-                                    {
-                                        if (!empty($_SESSION['username']))
-                                            {
-                                                echo 'Bonjour ' . $_SESSION['username'] . ' !<br />';
-                                            }
-                                        else 
-                                            {
-                                                throw new Exception('Votre identifiant n\'existe plus');
-                                            }
-                                    }
-                                catch (Exception $e)
-                                    {
-                                        echo 'Erreur : ' . $e->getMessage();
-                                    }
-                                ;?>
-                                <img src="/public/images/disconnect.png">
-                            </button>
-                            <div class="dropdown-menu" aria-labelledby="dropdownMenuOffset">
-                                <div>
-                                    <a class="dropdown-item" href="/public/index.php?action=disconnectTheAdmin">Déconnexion</a>
-                                </div>
-                            </div>
-                    </div> 
-                </div>
-                <!--message d'erreur si pas déconnecté-->
-                <div id="errorMessageDisplay">
-                    <p>
-                        <?php if (isset($error))
-                                {
-                                    echo $error;
-                                }
-                        ?>
-                    </p>
-                </div>
-            </div>
-        </nav>
+                        </li>
+                    <?php endif; ?>
+                <?php else: ?>
+                        <li>
+                            <a class="nav-link" href="/connectform">Connexion</a>
+                        </li>
+                <?php endif; ?>
+            </ul>
+            <form  action="/searchPosts" method="post" class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" name="userSearch" aria-label="Search">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+            </form>
         </div>
-    </div>
-    
-</header> 
-
-<!--script permettant le hover sur les dropdown-->
-<script>
-    $('ul.navbar-nav li.nav-item').hover(function() 
-        {
-            $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
-        }, function() 
-        {
-            $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
-        });
-</script>
+    </nav>
+<!--</header>-->
 
 
